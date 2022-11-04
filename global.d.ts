fafa50c1e54bcc6857b41bd7fc465e39cb520c45
youliangdao <youliangdaojing5@gmail.com>
2022-11-04 20:18:49 +0900

    ✨feat: FirebaseプロジェクトとReactの連携設定

---
+++ global.d.ts
@@ -0,0 +1,11 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    REACT_APP_FIREBASE_APIKEY: string;
+    REACT_APP_FIREBASE_APP_ID: string;
+    REACT_APP_FIREBASE_DATABASE: string;
+    REACT_APP_FIREBASE_DOMAIN: string;
+    REACT_APP_FIREBASE_PROJECTID: string;
+    REACT_APP_FIREBASE_SENDER_ID: string;
+    REACT_APP_FIREBASE_STORAGE_BUCKET: string;
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import "./index.css";
 
-const container = document.getElementById('root')!;
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+
+import App from "./App";
+import { store } from "./app/store";
+import reportWebVitals from "./reportWebVitals";
+
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
