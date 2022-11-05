f95556e49a41f19046502d09902dfde5b7939acf
youliangdao <youliangdaojing5@gmail.com>
2022-11-05 09:13:49 +0900

    ✨feat: サインイン画面を作成し、Googleアカウント認証機能を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React, { FC, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { auth } from "../firebase";
 import styles from "./App.module.css";
 import Auth from "./components/Auth";
 import Feed from "./components/Feed";
 import { login, logout, selectUser } from "./features/userSlice";
+import { auth } from "./firebase";
 
 const App: FC = () => {
   const user = useSelector(selectUser);
--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,7 +1,116 @@
-import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import React, { FC } from "react";
 
-const Auth = () => {
-  return <div>Auth</div>;
+import { auth, provider } from "../firebase";
+
+const useStyles = makeStyles((theme) => ({
+  avatar: {
+    backgroundColor: theme.palette.secondary.main,
+    margin: theme.spacing(1),
+  },
+  form: {
+    marginTop: theme.spacing(1),
+    width: "100%", // Fix IE 11 issue.
+  },
+  image: {
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundImage: "url(https://source.unsplash.com/random)",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+  },
+  paper: {
+    alignItems: "center",
+    display: "flex",
+    flexDirection: "column",
+    margin: theme.spacing(8, 4),
+  },
+  root: {
+    height: "100vh",
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const Auth: FC = () => {
+  const classes = useStyles();
+  const signInGoogle = async () => {
+    await auth.signInWithPopup(provider).catch((error) => {
+      alert(error.messages);
+    });
+  };
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              // eslint-disable-next-line jsx-a11y/no-autofocus
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={signInGoogle}
+            >
+              SignIn With Google
+            </Button>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Auth;
